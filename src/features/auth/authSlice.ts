f29f366578f64829e1ddcce3eca1c1fd40b5860d
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-01-30 14:34:49 +0900

    authにおける非同期関数作成

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,158 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import axios from "axios";
+import { PROPS_AUTHEN, PROPS_NICK_NAME, PROPS_PROFILE } from "../types";
+
+const apiUrl = process.env.REACT_APP_DEV_API_URL;
+
+export const fetchAsyncLogin = createAsyncThunk(
+  "auth/post", //action name
+  async (authen: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}authen/jwt/create`, authen, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+);
+
+// 新規アカウント作成
+export const fetchAsyncLegister = createAsyncThunk(
+  "auth/register",
+  async (authen: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}api/register/`, authen, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+);
+
+// 新規プロフィール作成
+export const fetchAsyncCreateProf = createAsyncThunk(
+  "profile/post",
+  async (profile: PROPS_PROFILE) => {
+    const uploadData = new FormData();
+    uploadData.append("nickName", profile.nickName);
+    profile.img && uploadData.append("img", profile.img, profile.img.name);
+    const res = await axios.put(
+      `${apiUrl}api/profile/${profile.id}/`,
+      uploadData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+export const fetchAsyncUpdateProf = createAsyncThunk(
+  "profile/post",
+  async (nickName: PROPS_NICK_NAME) => {
+    const res = await axios.post(`${apiUrl}api/profile/`, nickName, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncGetProf = createAsyncThunk("profile/get", async () => {
+  const res = await axios.get(`${apiUrl}api/myprofile/`, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data[0]; // 配列で返却されるため、０番目の要素を取得
+});
+
+export const fetchAsyncGetProfs = createAsyncThunk("profiles/get", async () => {
+  const res = await axios.get(`${apiUrl}api/profile/`, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    openSignIn: true, //for login modal
+    openSignUp: false, //for register modal
+    openProfile: false, //for profile modal
+    isLoadingAuth: false,
+    myprofile: {
+      id: 0,
+      nickName: "",
+      userProfile: 0,
+      created_on: "",
+      img: "",
+    },
+    profiles: [
+      {
+        id: 0,
+        nickName: "",
+        userProfile: 0,
+        created_on: "",
+        img: "",
+      },
+    ],
+  },
+
+  reducers: {
+    // Auth時に表示するローディングを管理する
+    // API実行時にtrue/falseを切り替える
+    fetchCredStart(state) {
+      state.isLoadingAuth = true;
+    },
+    fetchCredEnd(state) {
+      state.isLoadingAuth = false;
+    },
+    setOpenSignIn(state) {
+      state.openSignIn = true;
+    },
+    resetOpenSignIn(state) {
+      state.openSignIn = false;
+    },
+    setOpenSignUp(state) {
+      state.openSignUp = true;
+    },
+    resetOpenSignUp(state) {
+      state.openSignUp = false;
+    },
+    setOpenProfile(state) {
+      state.openProfile = true;
+    },
+
+    resetOpenProfile(state) {
+      state.openProfile = false;
+    },
+    editNickName(state, action) {
+      state.myprofile.nickName = action.payload;
+    },
+  },
+});
+
+export const {
+  fetchCredStart,
+  fetchCredEnd,
+  setOpenSignIn,
+  resetOpenSignIn,
+  setOpenSignUp,
+  resetOpenSignUp,
+  setOpenProfile,
+  resetOpenProfile,
+  editNickName,
+} = authSlice.actions;
+
+export const selectCount = (state: RootState) => state.auth.value;
+
+export default authSlice.reducer;
