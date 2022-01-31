39f35ac475f35bb1649264c767d96317d10fa71c
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-01-31 11:49:41 +0900

    Authコンポーネントからの遷移先を作成

--- src/features/core/Core.tsx
+++ src/features/core/Core.tsx
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import Auth from "../auth/Auth";
+import styles from "./Core.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  Grid,
+  Avatar,
+  Badge,
+  CircularProgress,
+} from "@material-ui/core";
+
+import { MdAddAPhoto } from "react-icons/md";
+
+import {
+  editNickname,
+  selectProfile,
+  selectIsLoadingAuth,
+  setOpenSignIn,
+  resetOpenSignIn,
+  setOpenSignUp,
+  resetOpenSignUp,
+  setOpenProfile,
+  resetOpenProfile,
+  fetchAsyncGetProf,
+  fetchAsyncGetProfs,
+} from "../auth/authSlice";
+
+import {
+  selectPosts,
+  selectIsLoadingPost,
+  setOpenNewPost,
+  resetOpenNewPost,
+  fetchAsyncGetPosts,
+  fetchAsyncGetComments,
+} from "../post/postSlice";
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "$ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}))(Badge);
 
 const Core: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const profile = useSelector(selectProfile);
+  const posts = useSelector(selectPosts);
+  const isLoadingPost = useSelector(selectIsLoadingPost);
+  const isLoadingAuth = useSelector(selectIsLoadingAuth);
+
+  // 初回コンポーネントレンダリング時に、既にストレージにトークンが存在する場合、
+  // サインインのモーダルを表示しないようOpenSignInをfalseとし、プロフィール、投稿情報を取得する
+  useEffect(() => {
+    const fetchBootLoader = async () => {
+      if (localStorage.localJWT) {
+        dispatch(resetOpenSignIn());
+        const result = await dispatch(fetchAsyncGetProf());
+        if (fetchAsyncGetProf.rejected.match(result)) {
+          dispatch(setOpenSignIn());
+          return null;
+        }
+        await dispatch(fetchAsyncGetPosts());
+        await dispatch(fetchAsyncGetProfs());
+        await dispatch(fetchAsyncGetComments());
+      }
+    };
+    fetchBootLoader();
+  }, [dispatch]); // 警告を避けるため、dispatchとする。[] と同義。typescriptのdisableは統一的に行わない
+
   return (
     <div>
       <Auth />
